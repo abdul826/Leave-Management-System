54983eb12b752f5ee59e41dca3d463c1fb49fbf6
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2025-09-26 00:11:30 +0530

    Update helper.js
    update the real url of backend

--- FO/lms/src/API/helper.js
+++ FO/lms/src/API/helper.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8000/api';
+// export const BASE_URL = 'http://localhost:8000/api';
+export const BASE_URL = 'https://leave-management-system-akez.onrender.com/api/fetchleavetype';
