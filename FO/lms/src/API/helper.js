eec4696fdedd66b8f25d80b0c43804715fd0cd29
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2025-09-26 00:11:47 +0530

    Update helper.js

--- FO/lms/src/API/helper.js
+++ FO/lms/src/API/helper.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000/api';
-export const BASE_URL = 'https://leave-management-system-akez.onrender.com/api/fetchleavetype';
+export const BASE_URL = 'https://leave-management-system-akez.onrender.com';
