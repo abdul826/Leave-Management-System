795bb093e16f9018feb41a3b0b399a630fc2574f
Abdul Rahman <59992187+abdul826@users.noreply.github.com>
2025-09-26 00:21:24 +0530

    Update util.js
    Update the JWT token time

--- helper/util.js
+++ helper/util.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId)=>{
-    return jwt.sign({_id:userId}, process.env.JWT_SECRET_KEY, {expiresIn:"30s"});
+    return jwt.sign({_id:userId}, process.env.JWT_SECRET_KEY, {expiresIn:"1h"});
 }
 
 module.exports = generateToken;
